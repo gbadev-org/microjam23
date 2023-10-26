f3c9d95b975ac5d5b2c18698540040667bd3f523
GValiente <gustavo.valiente@protonmail.com>
2023-10-26 10:04:07 +0200

    row row row! by noah nelson merged

---
+++ include/row_water_animation.h
@@ -0,0 +1,66 @@
+#include "bn_regular_bg_items_row_water0101.h"
+#include "bn_regular_bg_items_row_water0102.h"
+#include "bn_regular_bg_items_row_water0103.h"
+#include "bn_regular_bg_items_row_water0104.h"
+#include "bn_regular_bg_items_row_water0105.h"
+#include "bn_regular_bg_items_row_water0106.h"
+#include "bn_regular_bg_items_row_water0107.h"
+#include "bn_regular_bg_items_row_water0108.h"
+#include "bn_regular_bg_items_row_water0109.h"
+#include "bn_regular_bg_items_row_water0110.h"
+#include "bn_regular_bg_items_row_water0111.h"
+#include "bn_regular_bg_items_row_water0112.h"
+#include "bn_regular_bg_items_row_water0113.h"
+#include "bn_regular_bg_items_row_water0114.h"
+#include "bn_regular_bg_items_row_water0115.h"
+#include "bn_regular_bg_items_row_water0116.h"
+#include "bn_regular_bg_items_row_water0117.h"
+#include "bn_regular_bg_items_row_water0118.h"
+#include "bn_regular_bg_items_row_water0119.h"
+#include "bn_regular_bg_items_row_water0120.h"
+#include "bn_regular_bg_items_row_water0121.h"
+#include "bn_regular_bg_items_row_water0122.h"
+#include "bn_regular_bg_items_row_water0123.h"
+#include "bn_regular_bg_items_row_water0124.h"
+#include "bn_regular_bg_items_row_water0125.h"
+#include "bn_regular_bg_items_row_water0126.h"
+#include "bn_regular_bg_items_row_water0127.h"
+#include "bn_regular_bg_items_row_water0128.h"
+#include "bn_regular_bg_items_row_water0129.h"
+#include "bn_regular_bg_items_row_water0130.h"
+
+namespace row::water_animation {
+constexpr unsigned N_FRAMES = 30;
+constexpr bn::regular_bg_item frames[] = {
+    bn::regular_bg_items::row_water0101,
+    bn::regular_bg_items::row_water0102,
+    bn::regular_bg_items::row_water0103,
+    bn::regular_bg_items::row_water0104,
+    bn::regular_bg_items::row_water0105,
+    bn::regular_bg_items::row_water0106,
+    bn::regular_bg_items::row_water0107,
+    bn::regular_bg_items::row_water0108,
+    bn::regular_bg_items::row_water0109,
+    bn::regular_bg_items::row_water0110,
+    bn::regular_bg_items::row_water0111,
+    bn::regular_bg_items::row_water0112,
+    bn::regular_bg_items::row_water0113,
+    bn::regular_bg_items::row_water0114,
+    bn::regular_bg_items::row_water0115,
+    bn::regular_bg_items::row_water0116,
+    bn::regular_bg_items::row_water0117,
+    bn::regular_bg_items::row_water0118,
+    bn::regular_bg_items::row_water0119,
+    bn::regular_bg_items::row_water0120,
+    bn::regular_bg_items::row_water0121,
+    bn::regular_bg_items::row_water0122,
+    bn::regular_bg_items::row_water0123,
+    bn::regular_bg_items::row_water0124,
+    bn::regular_bg_items::row_water0125,
+    bn::regular_bg_items::row_water0126,
+    bn::regular_bg_items::row_water0127,
+    bn::regular_bg_items::row_water0128,
+    bn::regular_bg_items::row_water0129,
+    bn::regular_bg_items::row_water0130,
+};
+}
