cdd78d17f3592154534c4b610b9e2589e32b9e37
GValiente <gustavo.valiente@protonmail.com>
2023-10-27 17:30:59 +0200

    required butano version bumped to 16.0.0

--- src/mj/mj_core.cpp
+++ src/mj/mj_core.cpp
@@ -2,13 +2,16 @@
 
 #include "bn_core.h"
 #include "bn_keypad.h"
-#include "bn_timers.h"
 #include "bn_version.h"
 
 #include "mj/mj_big_sprite_font.h"
 #include "mj/mj_build_config.h"
 #include "mj/mj_small_sprite_font.h"
 #include "mj/mj_sprite_font.h"
+
+#if MJ_VBLANK_ASSERT_ENABLED
+    #include "bn_timers.h"
+#endif
 
 namespace mj
 {
@@ -20,7 +23,7 @@ namespace
         return (version_major << 16) + (version_minor << 8) + patch;
     }
 
-    static_assert(version(BN_VERSION_MAJOR, BN_VERSION_MINOR, BN_VERSION_PATCH) >= version(15, 8, 2),
+    static_assert(version(BN_VERSION_MAJOR, BN_VERSION_MINOR, BN_VERSION_PATCH) >= version(16, 0, 0),
                   "Butano version is too old");
 }
 
