dfd5bb489c81ddfb3fbe6fcf2b37af541de3d5aa
Ben Johnson <benjohnson_321@msn.com>
2023-10-30 09:46:56 +0000

    Microjam Entry - Skull Catch (#8)
    * skull catch microgame added
    
    * Skull object added to Skelebro
    
    * Skull Throwing
    
    * Credits and dance timing
    
    * GFX: Outlines
    
    * Auto Throw and skull bounce
    
    * SkullCatch: Catching Implemented
    
    * SkullCatch: Dashing + SFX
    
    * SkullCatch: Music added (TOTSNUK17)

---
+++ include/pyro_skull_catch.h
@@ -0,0 +1,140 @@
+#ifndef TMG_TEST_GAME_H
+#define TMG_TEST_GAME_H
+
+#include "bn_regular_bg_ptr.h"
+#include "bn_sprite_ptr.h"
+#include "bn_fixed_point.h"
+#include "bn_fixed.h"
+
+#include "mj/mj_game.h"
+
+namespace pyro_sc
+{
+
+using namespace bn; //Style guide, shmyle guide!
+
+const fixed floorY = fixed(50);
+
+class Skull{
+public:
+	Skull(fixed_point p);
+	
+	void update(const mj::game_data& data);
+	
+	void toss(fixed_point v){
+		vel = v;
+		active = true;
+	}
+	
+	fixed_point offset;
+	bool active;
+	bool cracked;
+	bool missed;
+	bool caught;
+	fixed_point pos;
+
+private:
+	fixed_point vel;
+	fixed angle;
+	fixed angle_vel;
+	sprite_ptr sprite;
+};
+
+class Skelebro{
+public:
+	Skelebro(fixed_point p, bool hflip);
+	
+	void update(const mj::game_data& data);
+	
+	void throwSkull();
+	
+	bool failed()
+	{
+		return skull.cracked;
+	}
+	
+	fixed maxFlipTimer;
+	fixed flipTimer;
+	int timer;
+	Skull skull;
+
+private:
+	fixed_point pos;
+	sprite_ptr sprite_body;
+	bool throwing;
+};
+
+class skullCatch : public mj::game
+{
+
+public:
+	skullCatch(int completed_games, const mj::game_data& data);
+	
+	[[nodiscard]] string<16> title() const final
+	{
+		switch (_target)
+		{
+		case 0:
+			//This shouldn't be possible, free win it happens though.
+			return "Do Whatever";
+			break;
+		
+		case 1:
+			return "Get A Head!";
+			break;
+		
+		case 2:
+			return "Double Up!";
+			break;
+		
+		case 3:
+			return "Catch 3 Skulls!";
+			break;
+		
+		case 4:
+			return "Catch 4 Skulls!";
+			break;
+		
+		default:
+			// This game is impossible if this ever happens, so it shouldn't.
+			return "SOMETHING IS WRONG";
+			break;
+		}
+	}
+	
+	[[nodiscard]] int total_frames() const final
+	{
+		return _total_frames;
+	}
+	
+	void fade_in(const mj::game_data& data) final;
+	
+	[[nodiscard]] mj::game_result play(const mj::game_data& data) final;
+	
+	[[nodiscard]] bool victory() const final
+	{
+		return _catch_count >= _target;
+	}
+	
+	void fade_out(const mj::game_data& data) final;
+
+private:
+	regular_bg_ptr _bg;
+	int _total_frames;
+	int _show_result_frames = 60;
+	bool _defeat = false;
+	fixed _player_x = 0;
+	fixed _player_catch_x_offset = 0;
+	fixed _player_catch_y = fixed(23);
+	int _player_anim_timer = 0;
+	int _player_footstep_timer = 0;
+	int _player_anim_frame = 0;
+	Skelebro _skelebros[4];
+	sprite_ptr _player_sprite;
+	int _target = 0;
+	int _catch_count = 0;
+};
+
+}
+
+#endif
---
+++ src/pyro_skull_catch.cpp
@@ -0,0 +1,340 @@
+#include "pyro_skull_catch.h"
+
+// I don't know which ones I do and don't need anymore, here's all the ones the sprites example uses.
+#include "bn_core.h"
+#include "bn_math.h"
+#include "bn_keypad.h"
+#include "bn_display.h"
+#include "bn_blending.h"
+#include "bn_bg_palettes.h"
+#include "bn_regular_bg_ptr.h"
+#include "bn_sprites_mosaic.h"
+#include "bn_sprite_actions.h"
+#include "bn_sprite_builder.h"
+#include "bn_sprite_text_generator.h"
+#include "bn_sprite_animate_actions.h"
+#include "bn_sprite_first_attributes.h"
+#include "bn_sprite_third_attributes.h"
+#include "bn_sprite_position_hbe_ptr.h"
+#include "bn_sprite_first_attributes_hbe_ptr.h"
+#include "bn_sprite_third_attributes_hbe_ptr.h"
+#include "bn_sprite_affine_second_attributes.h"
+#include "bn_sprite_regular_second_attributes.h"
+#include "bn_sprite_affine_second_attributes_hbe_ptr.h"
+#include "bn_sprite_regular_second_attributes_hbe_ptr.h"
+#include "bn_fixed.h"
+
+#include "mj/mj_game_list.h"
+
+#include "bn_regular_bg_items_pyro_skullcatch_stage.h"
+#include "bn_sprite_items_pyro_skeletonbody.h"
+#include "bn_sprite_items_pyro_skeletonhead.h"
+
+#include "bn_sound_items.h"
+namespace
+{
+	constexpr bn::string_view code_credits[] = { "PyroPyro" };
+	constexpr bn::string_view graphics_credits[] = { "PyroPyro" };
+	constexpr bn::string_view sfx_credits[] = { "PyroPyro" };
+}
+
+MJ_GAME_LIST_ADD(pyro_sc::skullCatch)
+MJ_GAME_LIST_ADD_CODE_CREDITS(code_credits)
+MJ_GAME_LIST_ADD_GRAPHICS_CREDITS(graphics_credits)
+MJ_GAME_LIST_ADD_SFX_CREDITS(sfx_credits)
+
+namespace pyro_sc
+{
+
+void approach(fixed &val, fixed target, fixed vel)
+{
+	if(val > target)
+	{
+		val -= vel;
+		if(val < target)
+		{
+			val = target;
+		}
+	}
+	else if(val < target)
+	{
+		val += vel;
+		if(val > target)
+		{
+			val = target;
+		}
+	}
+}
+
+Skull::Skull(fixed_point p):
+	sprite(sprite_items::pyro_skeletonhead.create_sprite(p.x(),p.y()))
+{
+	active = false;
+	pos = p;
+	vel = fixed_point(0,0);
+	angle = 0;
+	angle_vel = 1;
+	cracked = false;
+	missed = false;
+	caught = false;
+	sprite.set_bg_priority(1);
+}
+
+void Skull::update(const mj::game_data& data)
+{
+	if(active)
+	{
+		fixed velX = vel.x();
+		approach(velX, fixed(0), fixed(0.01));
+		vel.set_x(velX);
+		vel.set_y(vel.y() + fixed(0.1));
+		pos = pos + vel;
+		sprite.set_position(pos);
+		if(pos.y() >= floorY && !cracked)
+		{
+			sound_items::pyro_skullcatch_drop.play(1, data.random.get_fixed(0.9, 1.1), clamp(pos.x()/100,fixed(-1),fixed(1)));
+			vel.set_y(vel.y() * fixed(-0.5));
+			cracked = true;
+			sprite.set_tiles(sprite_items::pyro_skeletonhead.tiles_item().create_tiles(1));
+		}
+		//angle += angle_vel;
+		//while(angle > fixed(360))
+		//{
+		//	angle -= fixed(360);
+		//}
+		//sprite.set_rotation_angle(angle);
+	}
+	else
+	{
+		sprite.set_position(pos + offset);
+	}
+	
+}
+
+Skelebro::Skelebro(fixed_point p, bool hflip) :
+	sprite_body(sprite_items::pyro_skeletonbody.create_sprite(p.x(),p.y())),
+	skull(Skull(fixed_point(p.x(),p.y()-15)))
+{
+	flipTimer = maxFlipTimer;
+	sprite_body.set_horizontal_flip(hflip);
+	throwing = false;
+	pos = p;
+	timer = -1;
+	
+	sprite_body.set_bg_priority(3);
+}
+
+void Skelebro::throwSkull()
+{
+	if(!throwing)
+	{
+		throwing = true;
+		timer = maxFlipTimer.integer();
+		sprite_body.set_tiles(sprite_items::pyro_skeletonbody.tiles_item().create_tiles(1));
+	}
+}
+
+void Skelebro::update(const mj::game_data& data)
+{
+	if(throwing)
+	{
+		timer--;
+		if(timer == 0)
+		{
+			sprite_body.set_tiles(sprite_items::pyro_skeletonbody.tiles_item().create_tiles(2));
+			fixed velX = data.random.get_fixed(fixed(-0.5),fixed(2));
+			if(pos.x() > 0)
+			{
+				velX = -velX;
+			}
+			skull.toss(fixed_point(velX,fixed(-4)));
+			sound_items::pyro_skullcatch_throw.play(1, data.random.get_fixed(0.9, 1.1), pos.x()/100);
+		}
+	}
+	else
+	{
+		if (timer > 0)
+		{
+			timer--;
+			if (timer <= 0)
+			{
+				throwSkull();
+			}
+		}
+	}
+	
+	flipTimer -= fixed(1);
+	if(flipTimer <= fixed(0))
+	{
+		flipTimer += maxFlipTimer;
+		sprite_body.set_horizontal_flip(!sprite_body.horizontal_flip());
+		if (!skull.caught)
+		{
+			if(sprite_body.horizontal_flip())
+			{
+				skull.offset.set_x(1);
+			}
+			else
+			{
+				skull.offset.set_x(-1);
+			}
+		}
+	}
+	if(flipTimer >= (maxFlipTimer/2))
+	{
+		skull.offset.set_y(0);
+	}
+	else
+	{
+		skull.offset.set_y(1);
+	}
+	
+	skull.update(data);
+}
+
+skullCatch::skullCatch(int completed_games, const mj::game_data& data) :
+	_bg(regular_bg_items::pyro_skullcatch_stage.create_bg((256 - 240) / 2, (256 - 160) / 2)),
+	_total_frames(play_jingle(mj::game_jingle_type::TOTSNUK17, completed_games, data)),
+	_skelebros{
+		Skelebro(fixed_point(-92,23),false),
+		Skelebro(fixed_point(-41,19),true),
+		Skelebro(fixed_point(41,19),false),
+		Skelebro(fixed_point(92,23),true)
+	},
+	_player_sprite(sprite_items::pyro_skeletonbody.create_sprite(0,38))
+{
+	int actual_total_frames = _total_frames + 48;
+	
+	fixed beatLength = fixed(actual_total_frames)/16;
+	
+	_skelebros[0].maxFlipTimer = beatLength;
+	_skelebros[0].flipTimer = beatLength - 24;
+	_skelebros[0].timer = beatLength.integer()*3;
+	
+	_skelebros[1].maxFlipTimer = beatLength;
+	_skelebros[1].flipTimer = beatLength - 24;
+	_skelebros[1].timer = beatLength.integer()*5;
+	
+	_skelebros[2].maxFlipTimer = beatLength;
+	_skelebros[2].flipTimer = beatLength - 24;
+	_skelebros[2].timer = beatLength.integer()*7;
+	
+	_skelebros[3].maxFlipTimer = beatLength;
+	_skelebros[3].flipTimer = beatLength - 24;
+	_skelebros[3].timer = beatLength.integer()*9;
+	
+	_player_sprite.set_bg_priority(2);
+	
+	_player_x = fixed(0);
+	_player_catch_x_offset = fixed(0);
+	_player_catch_y = fixed(23);
+	_target = max(1,min(4, (completed_games+10)/10));
+	_catch_count = 0;
+	_player_footstep_timer = 1;
+}
+
+void skullCatch::fade_in([[maybe_unused]] const mj::game_data& data)
+{
+}
+
+mj::game_result skullCatch::play(const mj::game_data& data)
+{
+	mj::game_result result;
+	result.exit = data.pending_frames == 0;
+	
+	_skelebros[0].update(data);
+	_skelebros[1].update(data);
+	_skelebros[2].update(data);
+	_skelebros[3].update(data);
+	
+	if(_skelebros[0].failed() ||	_skelebros[1].failed() ||	_skelebros[2].failed() ||	_skelebros[3].failed())
+	{
+		_defeat = true;
+	}
+	
+	fixed speed = fixed(3);
+	bool dash = keypad::a_held() || keypad::b_held();
+	
+	if (dash)
+	{
+		speed = fixed(5);
+	}
+	
+	// Move the player
+	if(keypad::left_held())
+	{
+		_player_x-=speed;
+		_player_anim_timer--;
+		_player_footstep_timer--;
+	}
+	else if(keypad::right_held())
+	{
+		_player_x+=speed;
+		_player_anim_timer--;
+		_player_footstep_timer--;
+	}
+	else
+	{
+		_player_anim_timer = 3;
+	}
+	
+	//Detect a catch
+	for (int i = 0; i <= 3; i++)
+	{
+		if (_skelebros[i].skull.active && !_skelebros[i].skull.missed)
+		{
+			if (_skelebros[i].skull.pos.y() > _player_catch_y)
+			{
+				fixed player_catch_x = _player_x + _player_catch_x_offset;
+				if (abs((player_catch_x) - _skelebros[i].skull.pos.x()) <= fixed(10))
+				{
+					_skelebros[i].skull.caught = true;
+					_skelebros[i].skull.active = false;
+					_skelebros[i].skull.offset.set_x(_skelebros[i].skull.pos.x() - _player_x);
+					_skelebros[i].skull.offset.set_y(_player_catch_y);
+					_player_catch_y -= fixed(8);
+					_player_catch_x_offset = _skelebros[i].skull.offset.x();
+					sound_items::pyro_skullcatch_catch.play(1, fixed(1) + (fixed(0.1)*_catch_count), clamp(((player_catch_x)/100),fixed(-1),fixed(1)));
+					_catch_count++;
+				}
+				else
+				{
+					_skelebros[i].skull.missed = true;
+				}
+			}
+		}
+		
+		if (_skelebros[i].skull.caught)
+		{
+			_skelebros[i].skull.pos.set_x(_player_x);
+		}
+	}
+	
+	// Update Player sprite position
+	if(_player_anim_timer <= 0)
+	{
+		_player_sprite.set_horizontal_flip(!_player_sprite.horizontal_flip());
+		_player_anim_timer = 5;
+	}
+	if (_player_footstep_timer <= 0)
+	{
+		_player_footstep_timer = 12;
+		if (dash)
+		{
+			sound_items::pyro_skullcatch_footstep.play(0.5, 1.5, clamp(((_player_x)/100),fixed(-1),fixed(1)));
+		}
+		else
+		{
+			sound_items::pyro_skullcatch_footstep.play(0.5, 1, clamp(((_player_x)/100),fixed(-1),fixed(1)));
+		}
+	}
+	_player_sprite.set_x(_player_x.integer());
+	
+	return result;
+}
+
+void skullCatch::fade_out([[maybe_unused]] const mj::game_data& data)
+{
+}
+
+}
