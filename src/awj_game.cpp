b5ba40a9c1cea49b29788dc71be2673d83d0cc52
Jeremy Clarke <gecko@exelo.tl>
2023-10-30 00:37:46 +0000

    Fix uninitialised variable in awj candy game
    This would sometimes cause the bags to move
    immediately instead of waiting for the candy to enter.

--- src/awj_game.cpp
+++ src/awj_game.cpp
@@ -86,6 +86,7 @@ awj_game::awj_game([[maybe_unused]] const int completed_games, [[maybe_unused]] 
 	, next_bags{ 0, 1, 2, 3, 4 }
 {
 	current_state = 0;
+	go_to_next_state = false;
 	state_time = 0;
 
 	const uint8_t difficulty = static_cast<uint8_t>(recommended_difficulty_level(completed_games, data));
